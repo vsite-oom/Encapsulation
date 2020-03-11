0aa321f4f41932eca83577784ae0cb9f77fd7d74
Julijan Sribar <julijan.sribar@reversinglabs.com>
2020-03-11 16:24:49 +0100

    Implement simple version for two lines

--- Line/Program.cs
+++ Line/Program.cs
@@ -3,12 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Line
+namespace Vsite.Oom
 {
     class Program
     {
         static void Main(string[] args)
         {
+            try
+            {
+                double k1 = double.Parse(args[0]);
+                double l1 = double.Parse(args[1]);
+                double k2 = double.Parse(args[2]);
+                double l2 = double.Parse(args[3]);
+
+                if (k1 == k2)
+                    Console.WriteLine("Lines are paralel");
+                else
+                {
+                    double x = (l2 - l1) / (k1 - k2);
+                    double y = k1 * x + l1;
+                    Console.WriteLine($"Intersection: ({x}, {y})");
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid entry");
+            }
         }
     }
 }

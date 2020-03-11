9eb243dc8465c850d164b2f35ba34ed98aaabe43
Fran Kalinger <frkaling@vsite.hr>
2020-03-11 20:23:02 +0100

    Done

--- Line/Program.cs
+++ Line/Program.cs
@@ -5,30 +5,64 @@ using System.Text;
 
 namespace Vsite.Oom
 {
+    class Point
+    {
+        public Point(double x,double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public readonly double x;
+        public readonly double y;
+    }
+    class Line
+    {
+        public Line(double k,double l)
+        {
+            this.k = k;
+            this.l = l;
+        }
+        private double k;
+        private double l;
+        public Point Intersection(Line other)
+        {
+            if (k == other.k)
+                return null;
+            double x = (other.l - l) / (k - other.k);
+            double y = k * x + l;
+            return new Point(x, y);
+            
+            
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
             try
-            {
-                double k1 = double.Parse(args[0]);
-                double l1 = double.Parse(args[1]);
-                double k2 = double.Parse(args[2]);
-                double l2 = double.Parse(args[3]);
+            { 
 
-                if (k1 == k2)
-                    Console.WriteLine("Lines are paralel");
-                else
+
+                
+
+                Line l1 = new Line(1, 0);
+                Line l2 = new Line(2, -1);
+                Line l3 = new Line(3, -4);
+                var p12 = l1.Intersection(l2);
+                if (p12 == null)
                 {
-                    double x = (l2 - l1) / (k1 - k2);
-                    double y = k1 * x + l1;
-                    Console.WriteLine($"Intersection: ({x}, {y})");
+                    Console.WriteLine("Lines are paralel");
                 }
+                else
+                Console.WriteLine($"Intersection: ({p12.x}, {p12.y})");
+
             }
             catch (Exception)
             {
                 Console.WriteLine("Invalid entry");
             }
         }
+
+       
     }
 }

6eac6220ead265ef71c453f8913905177ebff965
mvuki <mvuki@LAPTOP-7EO7DRN2>
2020-03-11 20:22:24 +0100

    Implement  OO line

--- Line/Program.cs
+++ Line/Program.cs
@@ -5,30 +5,71 @@ using System.Text;
 
 namespace Vsite.Oom
 {
+    class Point
+    {
+        public Point(double x,double y)
+        {
+            X = x;
+            Y = y;
+
+        }
+        public readonly double X;
+        public readonly double Y;
+
+
+    
+    }
+    
+    class Line
+    {
+        private double k;
+            private double l;
+        public Line(double k, double l)
+        {
+            this.k = k;
+            this.l = l;
+        }
+        public  Point Intersection(Line other)
+        {
+            if (k == other.k)
+                return null;
+          
+        double x = (other.l - l) / (k - other.k);
+        double y = k * x + l;
+        return new Point(x, y);
+      
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
             try
             {
-                double k1 = double.Parse(args[0]);
-                double l1 = double.Parse(args[1]);
-                double k2 = double.Parse(args[2]);
-                double l2 = double.Parse(args[3]);
-
-                if (k1 == k2)
+          
+                Line l1 = new Line(1, 0);
+                Line l2 = new Line(2, -1);
+                Line l3 = new Line(3, -4);
+                var p12=   l1.Intersection(l2);
+                if (p12 == null)
+                {
                     Console.WriteLine("Lines are paralel");
+
+                }
                 else
                 {
-                    double x = (l2 - l1) / (k1 - k2);
-                    double y = k1 * x + l1;
-                    Console.WriteLine($"Intersection: ({x}, {y})");
+                    Console.WriteLine($"Intersection:({p12.X},{p12.Y})");
+
                 }
+              
             }
             catch (Exception)
             {
                 Console.WriteLine("Invalid entry");
             }
         }
+
+      
     }
 }

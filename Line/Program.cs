04aa49040f7edae36aaf36ba750bd1cf884850e4
Paško Berišić <paberisi@vsite.hr>
2020-03-11 20:27:43 +0100

    Implement oo-line

--- Line/Program.cs
+++ Line/Program.cs
@@ -5,6 +5,41 @@ using System.Text;
 
 namespace Vsite.Oom
 {
+    class Point
+    {
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public readonly double X;
+        public readonly double Y;
+    }
+
+    class Line
+    {
+        public Line(double k, double l)
+        {
+            this.k = k;
+            this.l = l;
+        }
+
+        public Point Intersection(Line other)
+        {
+            if (k == other.k)
+                return null;
+
+            double x = (other.l - l) / (k - other.k);
+            double y = k * x + l;
+            return new Point(x, y);
+
+        }
+
+        private double k;
+        private double l;
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -15,8 +50,13 @@ namespace Vsite.Oom
                 double l1 = double.Parse(args[1]);
                 double k2 = double.Parse(args[2]);
                 double l2 = double.Parse(args[3]);
+                Line l1 = new Line(1, 0);
+                Line l2 = new Line(2, -1);
+                Line l3 = new Line(3, -4);
 
                 if (k1 == k2)
+                    var p12 = l1.Intersection(l2);
+                if (p12 == null)
                     Console.WriteLine("Lines are paralel");
                 else
                 {
@@ -24,6 +64,7 @@ namespace Vsite.Oom
                     double y = k1 * x + l1;
                     Console.WriteLine($"Intersection: ({x}, {y})");
                 }
+                Console.WriteLine($"Intersection: ({p12.X}, {p12.Y})");
             }
             catch (Exception)
             {

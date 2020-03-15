158eaefa611f6f9c0e9396dfa5481205a237ef3d
Paško Berišić <paberisi@vsite.hr>
2020-03-15 23:25:39 +0100

    Ispravak

--- Line/Program.cs
+++ Line/Program.cs
@@ -5,71 +5,64 @@ using System.Text;
 
 namespace Vsite.Oom
 {
-    class Point
+ class Program
     {
-        public Point(double x, double y)
+        class Point
         {
-            X = x;
-            Y = y;
+            public readonly double x;
+            public readonly double y;
+            public Point(double x, double y)
+            {
+                this.x = x;
+                this.y = y;
+            }
         }
-
-        public readonly double X;
-        public readonly double Y;
-    }
-
-    class Line
-    {
-        public Line(double k, double l)
+        class Line
         {
-            this.k = k;
-            this.l = l;
+            private double k;
+            private double l;
+            public Line (double k, double l)
+            {
+                this.k = k;
+                this.l = l;
+            }
+            public Point Intersection(Line other)
+            {
+                if (k == other.k)
+                    return null;
+                double x = (other.l - l) / (k - other.k);
+                double y = y = k * x + 1;
+                return new Point(x, y);
+            }
         }
-
-        public Point Intersection(Line other)
-        {
-            if (k == other.k)
-                return null;
-
-            double x = (other.l - l) / (k - other.k);
-            double y = k * x + l;
-            return new Point(x, y);
-
-        }
-
-        private double k;
-        private double l;
-    }
-
-    class Program
-    {
         static void Main(string[] args)
         {
             try
             {
-                double k1 = double.Parse(args[0]);
-                double l1 = double.Parse(args[1]);
-                double k2 = double.Parse(args[2]);
-                double l2 = double.Parse(args[3]);
                 Line l1 = new Line(1, 0);
                 Line l2 = new Line(2, -1);
                 Line l3 = new Line(3, -4);
-
-                if (k1 == k2)
-                    var p12 = l1.Intersection(l2);
-                if (p12 == null)
-                    Console.WriteLine("Lines are paralel");
+                var p12 = l1.Intersection(l2);
+                if(p12 == null)
+                    Console.WriteLine("Linije su paralelne");
                 else
-                {
-                    double x = (l2 - l1) / (k1 - k2);
-                    double y = k1 * x + l1;
-                    Console.WriteLine($"Intersection: ({x}, {y})");
-                }
-                Console.WriteLine($"Intersection: ({p12.X}, {p12.Y})");
+                    Console.WriteLine($"Intersection: ({p12.x}, {p12.y})");
             }
-            catch (Exception)
+            catch(Exception)
             {
-                Console.WriteLine("Invalid entry");
+                Console.WriteLine("Pogrešan unos");
             }
         }
-    }
+        private static void Intersection (double k1, double k2, double l1, double l2)
+        {
+            if ( k1 == k2)
+                Console.WriteLine("Linije su paralelne");
+            else
+            {
+                double x = (l2 - l1) / (k1 - k2);
+                double y = k1 * x + l1;
+                Console.WriteLine($"Intersection: ({x}, {y})");
+            }
+        }
+    }   
 }
